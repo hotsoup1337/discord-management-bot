2c1f92b23966ccb1702aff8b8e34278c48801efd
Hot Soup <benjaminfinck02@googlemail.com>
2023-02-28 20:49:56 +0100

    Update grade_overview.py
    implemented basic class SelectMenuLesson
    
    need to work on saving the selected lesson with user and put together with grade select.
    
    maybe JSON file?

--- cogs/grade_overview.py
+++ cogs/grade_overview.py
@@ -1,1 +1,17 @@
 import discord
+from discord import app_commands
+from discord.ext import commands
+
+import mysql.connector
+import os
+
+
+class SelectMenuLesson():
+    def __init__(self):
+        super().__init__(placeholder="Wähle ein Lernfeld aus.", max_values=1, min_values=1)
+        # add for loop -> every lesson in database and add an option
+        # label = lesson name
+        # description = teacher form_of_address + name
+        self.add_option(label="Lernfeld Test", description="Herr Mustermann")
+
+        # how to save the selected lesson together with the selected grade?
