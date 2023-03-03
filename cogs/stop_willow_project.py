2a0358e1cc9f94ed8c7f13319fe06231d57ad661
Benjamin <benjaminfinck02@googlemail.com>
2023-03-03 07:17:27 +0000

    Updated stop_willow_project.py

---
+++ cogs/stop_willow_project.py
@@ -0,0 +1,37 @@
+import discord 
+from discord import app_commands
+from discord.ext import commands
+
+
+class LinkButton(discord.ui.Button):
+    def __init__(self, text, buttonStyle, bot):
+        super().__init__(label=text, style=buttonStyle)
+        self.bot = bot
+
+    async def callback(self, interaction: discord.Interaction):
+
+        await interaction.response.defer()
+
+
+class LinkView(discord.ui.View):
+    def __init__(self, bot):
+        super().__init__(timeout=None)
+        self.add_item(LinkButton("Change.org", discord.ButtonStyle.primary, bot))
+
+class Messages(commands.Cog):
+
+    def __init__(self):
+        self.bot = bot
+
+    @app_commands.command(name="stop-willow", description="Share the petition for stop willow")
+    @app_commands.checks.has_role("MET 11")
+    async def stop_willow(self, interaction: discord.Interaction):
+        if isinstance(error, app_commands.MissingRole):
+            await interaction.response.send_message(str("Missing permissions!", ephmeral=True, delete_after=3))
+
+        else:
+            await interaction.response.send_message(view=LinkView(self.bot))
+
+    
+        
+    
