c34e9aca9acf6f9e4d21de5c1a31967c30ec7fc2
Hot Soup <benjaminfinck02@googlemail.com>
2023-03-08 18:16:21 +0100

    Update user_stats.py

--- follow_system/user_stats.py
+++ follow_system/user_stats.py
@@ -10,18 +10,18 @@ class user_statsMenu(discord.ui.Select):
     def __init__(self):
         super().__init__(placeholder="Wähle einen Nutzer aus", max_values=1, min_values=1)
 
-        async def callback(self, interaction: discord.Interaction):
+    async def callback(self, interaction: discord.Interaction):
 
-            mydb = mysql.connector.connect(
-                host=os.getenv("DB.USER"),
-                user=os.getenv("DB.USER"),
-                password=os.getenv("DB.PW"),
-                database=os.getenv("DB")
-            )
+        mydb = mysql.connector.connect(
+            host=os.getenv("DB.USER"),
+            user=os.getenv("DB.USER"),
+            password=os.getenv("DB.PW"),
+            database=os.getenv("DB")
+        )
 
-            members = interaction.guild.fetch_members()
+        members = interaction.guild.fetch_members()
 
-            print(members)
+        print(members)
 
 
 
@@ -29,14 +29,14 @@ class user_statsMenu(discord.ui.Select):
 class user_statsView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
-        self.add_item(user_statsView)
+        self.add_item(user_statsMenu())
 
 class setup_user_stats(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @app_commands.command(name="user_stats", description="Statistiken eines Nutzers anzeigen")
-    async def user_stats(self, interaction: discord.Interaction):
+    async def user_stats(self, interaction: discord.Interaction, user: discord.Member):
 
         await interaction.response.send_message(view=user_statsView)
 
